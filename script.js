caffdefb7788d30eaca3fed5aa5e77e872573ded
Abdullah Ramadan <74230257+Abdullahali310@users.noreply.github.com>
2022-12-01 13:16:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let inputTextArea = document.getElementById("input-texterea");
+let characterCount = document.getElementById("character-count");
+let wordCount = document.getElementById("word-count");
+
+
+inputTextArea.addEventListener("input", () => {
+    characterCount.textContent = inputTextArea.value.length;
+    let txt = inputTextArea.value.trim();
+    wordCount.textContent = txt.split(/\s+/).filter((item) =>
+     item).length;
+});
+
